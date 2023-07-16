07774b77cfb4efb8ba2086978601883c75df34d0
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-07-16 11:23:08 +0300

    Hero Component

--- 01-react-facts/src/App.jsx
+++ 01-react-facts/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import './App.css';
 import Header from './Header';
+import Hero from './Hero';
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
---
+++ 01-react-facts/src/Hero.jsx
@@ -0,0 +1,18 @@
+function Hero() {
+  return (
+    <main className="hero">
+      <div className="container hero--container">
+        <h1 className="hero__heading">Fun facts about React</h1>
+        <ul className="hero__list">
+          <li className="hero__item">Was first released in 2013</li>
+          <li className="hero__item">Was originally created by Jordan Walke</li>
+          <li className="hero__item">Has well over 100k stars on GitHub</li>
+          <li className="hero__item">Is maintained by Facebook</li>
+          <li className="hero__item">Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default Hero;
